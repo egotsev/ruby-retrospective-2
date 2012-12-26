8590cf49240dbb6285598324dedac9c7780cf09a
Emil Gotsev <emo@peleto.(none)>
2012-12-26 16:28:42 +0200

    Homework 1 totally rewritten.

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,63 +1,33 @@
 class Integer	
-	def prime_divisors
-		array = []
-		pos_self = self.abs
-		pos_self.downto(2) { |n| array << n if pos_self % n == 0 and n.prime? }
-		array.sort!
-		return array unless array.empty?
-		nil
-	end
+  def prime_divisors
+    2.upto(abs).select { |n| self % n == 0 and n.prime? }
+  end
 
-	def prime?
-		is_prime = true
-		self.downto 2 do |n| 
-			is_prime = false if self % n == 0 and n != self
-		end
-		is_prime
-	end
+  def prime?
+    2.upto(abs / 2).all? { |n| self % n != 0 }
+  end
 end
 
 class Range
-	def fizzbuzz
-		array = []
-		self.each do |n|
-			if n % 3 == 0 and n % 5 == 0
-				array << :fizzbuzz
-			elsif n % 3 == 0
-				array << :fizz
-			elsif n % 5 == 0 
-				array << :buzz
-			else
-				array << n
-			end
-		end
-		array
-	end
+  def fizzbuzz
+    map do |n|
+      if n % 15 == 0 then :fizzbuzz
+      elsif n % 3 == 0 then :fizz
+      elsif n % 5 == 0 then :buzz
+      else n
+      end
+    end
+  end
 end
 
 class Hash
-	def group_values
-		values = Hash.new { |hash, key| hash[key]=[] }
-		self.keys.each do |n|
-			values[self[n]] << n
-		end
-		values
-	end
+  def group_values
+    keys.group_by { |key| self[key] }
+  end
 end
 
 class Array
-	def densities
-		self.collect do |item|
-			if item.is_a? Integer
-				return item
-			end
-			counter = 0
-			self.each do |n| 
-				if n == item
-					counter+=1
-				end
-			end
-			counter
-		end
-	end
+  def densities
+    map { |element| count element }
+  end
 end
