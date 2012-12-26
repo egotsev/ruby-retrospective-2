0777a24213a9096302197dff8d2371913e9aafe5
egotsev <egotsev@gmail.com>
2012-12-26 16:35:39 +0200

    Homework 2 rewritten in a more functional way, using blocks as argument to the constructor of Criteria

--- solutions/02.rb
+++ solutions/02.rb
@@ -2,19 +2,17 @@ class Song
   attr_accessor :name, :artist, :album
 
   def initialize(name, artist, album)
-    @name = name
-    @artist = artist
-    @album = album
+    @name, @artist, @album = name, artist, album
   end
 end
 
 class Collection
-  attr_accessor :songs
-
   include Enumerable
 
-  def initialize
-    @songs = []
+  attr_reader :songs
+
+  def initialize(songs)
+    @songs = songs
   end
 
   def each
@@ -22,108 +20,67 @@ class Collection
   end
 
   def Collection.parse(text)
-    new_collection = Collection.new
-    text.split(/\n+/).each_slice(3) do |slice|
-      new_collection.songs << Song.new(slice[0], slice[1], slice[2])
+    songs = text.split(/\n+/).each_slice(3).map do |slice|
+      Song.new(slice[0], slice[1], slice[2])
     end
-    new_collection
+    new songs
   end
 
   def artists
-    map { |song| song.artist }.uniq
+    map(&:artist).uniq
   end
 
   def albums
-    map { |song| song.album }.uniq
+    map(&:album).uniq
   end
 
   def names
-    map { |song| song.name }.uniq
+    map(&:name).uniq
   end
 
   def filter(criteria)
-    new_collection = Collection.new
-    new_collection.songs = select { |song| criteria.match? song }
-    new_collection
+    Collection.new select { |song| criteria.matches? song }
   end
 
   def adjoin(collection)
-    adjoined = Collection.new
-    adjoined.songs = self.songs | collection.songs
-    adjoined
+    Collection.new @songs | collection.songs
+  end
+
+  def |(collection)
+    adjoin collection
   end
 end
 
 class Criteria
-  def initialize(filter_string)
-    @filter_string = filter_string
+  def initialize(&block)
+    @filter_proc = block
   end
 
-  def self.name(filter_string)
-    NameCriteria.new(filter_string)
+  def self.name(name)
+    new { |song| song.name == name}
   end
 
-  def self.artist(filter_string)
-    ArtistCriteria.new(filter_string)
+  def self.artist(artist)
+    new { |song| song.artist == artist}
   end
 
-  def self.album(filter_string)
-    AlbumCriteria.new(filter_string)
+  def self.album(album)
+    new { |song| song.album == album}
+  end
+
+  def matches?(song)
+    @filter_proc.(song)
   end
 
   def &(other)
-    DoubleCriteria.new(self, other, :and)
+    Criteria.new { |song| matches?(song) and other.matches?(song)}
   end
 
   def |(other)
-    DoubleCriteria.new(self, other, :or)
+    Criteria.new { |song| matches?(song) or other.matches?(song)}
   end
 
   def !
-    ReversedCriteria.new(self)
-  end
-end
-
-class DoubleCriteria < Criteria
-  def initialize(first_criteria, second_criteria, operator)
-    @first_criteria = first_criteria
-    @second_criteria = second_criteria
-    @operator = operator
-  end
-
-  def match?(song)
-    if @operator == :and
-      @first_criteria.match?(song) & @second_criteria.match?(song)
-    elsif @operator == :or
-      @first_criteria.match?(song) | @second_criteria.match?(song)
-    end
-  end
-end
-
-class ReversedCriteria < Criteria
-  def initialize(criteria)
-    @criteria = criteria
-  end
-
-  def match?(song)
-    !@criteria.match? song
-  end
-end
-
-class NameCriteria < Criteria
-  def match?(song)
-    song.name.eql? @filter_string
-  end
-end
-
-class ArtistCriteria < Criteria
-  def match?(song)
-    song.artist.eql? @filter_string
-  end
-end
-
-class AlbumCriteria < Criteria
-  def match?(song)
-    song.album.eql? @filter_string
+    Criteria.new { |song| !matches?(song) }
   end
 end
